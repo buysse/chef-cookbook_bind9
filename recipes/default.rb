06a7c42a35f2b6d45b10aa0a7a7cc08df69e2189
Joshua Buysse <buysse@umn.edu>
2013-08-09 23:51:02 -0500

    Added (untested) support for slave zones.

--- recipes/default.rb
+++ recipes/default.rb
@@ -72,34 +72,37 @@ search(:zones).each do |zone|
     end
   end
 
-  template "#{node[:bind9][:config_path]}/#{zone['domain']}" do
-    source "#{node[:bind9][:config_path]}/#{zone['domain']}.erb"
-    local true
-    owner "root"
-    group "root"
-    mode 0644
-    notifies :restart, resources(:service => "bind9")
-    variables({
-      :serial => Time.new.strftime("%Y%m%d%H%M%S")
-    })
-    action :nothing
-  end
+  # only master domains require this file, for slave domains it will be created.
+  if zone["type"] == "master" then
+    template "#{node[:bind9][:config_path]}/#{zone['domain']}" do
+      source "#{node[:bind9][:config_path]}/#{zone['domain']}.erb"
+      local true
+      owner "root"
+      group "root"
+      mode 0644
+      notifies :restart, resources(:service => "bind9")
+      variables({
+        :serial => Time.new.strftime("%Y%m%d%H%M%S")
+      })
+      action :nothing
+    end
 
-  template "#{node[:bind9][:config_path]}/#{zone['domain']}.erb" do
-    source "zonefile.erb"
-    owner "root"
-    group "root"
-    mode 0644
-    variables({
-      :domain => zone['domain'],
-      :soa => zone['zone_info']['soa'],
-      :contact => zone['zone_info']['contact'],
-      :global_ttl => zone['zone_info']['global_ttl'],
-      :nameserver => zone['zone_info']['nameserver'],
-      :mail_exchange => zone['zone_info']['mail_exchange'],
-      :records => zone['zone_info']['records']
-    })
-    notifies :create, resources(:template => "#{node[:bind9][:config_path]}/#{zone['domain']}"), :immediately
+    template "#{node[:bind9][:config_path]}/#{zone['domain']}.erb" do
+      source "zonefile.erb"
+      owner "root"
+      group "root"
+      mode 0644
+      variables({
+        :domain => zone['domain'],
+        :soa => zone['zone_info']['soa'],
+        :contact => zone['zone_info']['contact'],
+        :global_ttl => zone['zone_info']['global_ttl'],
+        :nameserver => zone['zone_info']['nameserver'],
+        :mail_exchange => zone['zone_info']['mail_exchange'],
+        :records => zone['zone_info']['records']
+      })
+      notifies :create, resources(:template => "#{node[:bind9][:config_path]}/#{zone['domain']}"), :immediately
+    end
   end
 end
 
